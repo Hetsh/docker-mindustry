fb02baae2bf1ad5eda0a538f05eb49a15381b8a4
Hetsh <git@hetsh.de>
2020-08-06 12:26:42 +0200

    Implemented mindustry server updates

--- update.sh
+++ update.sh
@@ -20,20 +20,11 @@ assert_dependency "curl"
 update_image "library/alpine" "Alpine Linux" "false" "\d{8}"
 
 # Mindustry Server
-CURRENT_MC_VERSION="${_CURRENT_VERSION%-*}"
-NEW_MC_VERSION=$(curl -s -L "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".latest.release")
-if [ "$CURRENT_MC_VERSION" != "$NEW_MC_VERSION" ]; then
-	prepare_update "mindustry" "Mindustry Server" "$CURRENT_MC_VERSION" "$NEW_MC_VERSION"
-	update_version "$NEW_MC_VERSION"
-
-	METADATA_URL=$(curl -s -L "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".versions[] | select(.id==\"$NEW_MC_VERSION\") | .url")
-	DOWNLOAD_URL=$(curl -s -L "$METADATA_URL" | jq -r ".downloads.server.url")
-
-	# Since the minecraft server is not a regular package, the version number needs
-	# to be replaced with the url to download the binary
-	_UPDATES[-3]="BIN_URL"
-	_UPDATES[-2]="\".*\""
-	_UPDATES[-1]="\"$DOWNLOAD_URL\""
+CURRENT_APP_VERSION="${_CURRENT_VERSION%-*}"
+NEW_APP_VERSION=$(curl --silent --location "https://api.github.com/repos/Anuken/Mindustry/releases/latest" | jq -r ".tag_name" | sed "s/^v//")
+if [ "$CURRENT_APP_VERSION" != "$NEW_APP_VERSION" ]; then
+	prepare_update "APP_VERSION" "Mindustry Server" "$CURRENT_APP_VERSION" "$NEW_APP_VERSION"
+	update_version "$NEW_APP_VERSION"
 fi
 
 # OpenJRE
