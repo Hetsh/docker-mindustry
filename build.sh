c940484bfe380a1b72f6a7593d75a0ff97db0845
Hetsh <git@hetsh.de>
2020-08-25 12:59:18 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,18 +21,25 @@ fi
 
 # Build the image
 APP_NAME="mindustry"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 6567:6567/tcp \
-	--publish 6567:6567/udp \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_TAG"
-fi
+case "${1-}" in
+	"--test")
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 6567:6567/tcp \
+		--publish 6567:6567/udp \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
